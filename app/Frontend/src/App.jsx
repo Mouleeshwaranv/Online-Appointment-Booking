dd2b6593f1365928773683757efa1d0a34ee80f7
Mouleeshwaranv <mouleesh0805@gmail.com>
2025-11-22 00:36:40 +0530

    Add files via upload

---
+++ app/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <div className = "text-4xl text-blue-200">
+      Hello Moulee
+    </div>
+   
+  )
+}
+
+export default App
